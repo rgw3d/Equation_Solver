6da6dca419895b0d166e58a33ec9567ebf4a2816
rgw3d <rgw3d@aol.com>
2014-04-17 14:14:16 -0400

    Outline
    just an outline. no real content

---
+++ Nomials.java
@@ -0,0 +1,17 @@
+/**
+ *Just quickly outlining a few things and making the classes to actually edit later
+ *
+ */
+public class Nomials
+{
+    public Nomials(double numCount, double xCount, double exponentCount)
+    {
+        count=numCount;
+        nomial=xCount;
+        exponent=exponentCount;
+    }
+    private double count;
+    private double nomial;
+    private double exponent;
+    
+}
---
+++ Parenthesis.java
@@ -0,0 +1,14 @@
+/**
+ * Just outlining a few things
+ */
+public class Parenthesis
+{
+    public Parenthesis(boolean isOpen)
+    {
+        open=isOpen;
+        
+    }
+    private boolean open;
+    
+    
+}
